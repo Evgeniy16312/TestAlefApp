31cd91f2382363e479165dee8b66997d5b45950b
Evgeniy16312 <turin.92@mail.ru>
2022-02-07 20:52:53 +0300

    UI refactoring (add animation navigate to fragments)

--- app/src/main/java/com/example/testalefapp/presentation/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/testalefapp/presentation/gallery/GalleryFragment.kt
@@ -2,23 +2,39 @@ package com.example.testalefapp.presentation.gallery
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.example.testalefapp.R
 import com.example.testalefapp.databinding.FragmentGalleryBinding
 
+@Suppress("UNREACHABLE_CODE")
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentGalleryBinding.bind(view)
+        binding.button?.setOnClickListener {
+            animationNavigateToFragment()
+        }
+    }
+
+    private fun animationNavigateToFragment() {
+        val direction = GalleryFragmentDirections.actionGalleryFragmentToImageFragment()
+        findNavController().navigate(
+            direction,
+            // optional additional options, example of simple animation:
+            navOptions {
+                anim {
+                    enter = R.anim.enter
+                    exit = R.anim.exit
+                    popEnter = R.anim.pop_enter
+                    popExit = R.anim.pop_exit
+                }
+            }
+        )
     }
 }
